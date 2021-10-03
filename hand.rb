61be38534ecc97287e0c727c5d8456bf268eec2a
boy-com777 <gao.com1208@gmail.com>
2021-10-03 15:00:47 +0900

    全体処理：手札表示まで実装

--- hand.rb
+++ hand.rb
@@ -1,17 +1,32 @@
 require './message'
 
 class Hand
+  attr_accessor :show_hands
+
   # 初回山札から引く枚数を定義
   NUMBER_OF_HAND = 5
 
   def initialize
-    @hands []
+    @hands = []
   end
 
   def first_draw(deck)
     NUMBER_OF_HAND.times do
       card = deck.draw
       @hands << card
+    end
+
+    @show_hands = []
+    @hands.each do |hand|
+      @show_hands << hand
+    end
+  end
+
+  def hands_list
+    @hands.each.with_index(1) do |hand, i|
+      mark = hand[0]
+      number = hand[1]
+      puts "#{i}. #{mark}の#{number}"
     end
   end
 end
