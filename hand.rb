82ec904a0896b6411a36c95e1a10d67054eb2e17
boy-com777 <gao.com1208@gmail.com>
2021-10-03 14:21:01 +0900

    hand.rb 初回手札格納まで実装

--- hand.rb
+++ hand.rb
@@ -1,51 +1,17 @@
 require './message'
 
 class Hand
-  include Message
-
-  attr_accessor :show_hands
-
-  # 手札5枚引くのを定数で定義
+  # 初回山札から引く枚数を定義
   NUMBER_OF_HAND = 5
 
   def initialize
-    @hands = []
+    @hands []
   end
 
-  # 手札を山札から引く
-  def first_draw_player(deck)
+  def first_draw(deck)
     NUMBER_OF_HAND.times do
       card = deck.draw
       @hands << card
-    end
-
-    @show_hands = []
-    @hands.each do |hand|
-      @show_hands << hand
-    end
-  end
-
-  def hands_list_player
-    puts '=*=*=*=*= プレイヤー 手札 =*=*=*=*=*='
-    @hands.each.with_index(1) do |hand, i|
-      puts "#{i}. #{hand[0]}の#{hand[1]}"
-    end
-  end
-
-  def second_draw_player(deck)
-    information2
-    @change_number = gets.chomp.split(&:to_s)
-    @change = Regexp.new(/^\d$|\d\s/)
-    if @change =~ @change_number
-      @hands[0] = deck.draw if @change_number.include?('1')
-      @hands[1] = deck.draw if @change_number.include?('2')
-      @hands[2] = deck.draw if @change_number.include?('3')
-      @hands[3] = deck.draw if @change_number.include?('4')
-      @hands[4] = deck.draw if @change_number.include?('5')
-      @show_hands = []
-      @show_hands << @hands
-    else
-      information3
     end
   end
 end
